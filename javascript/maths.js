1bd35e3ae37b472ab72fbd52299e374c0859848e
amrutaadhav <amrutaadhav0727@gmail.com>
2024-07-21 00:08:59 +0530

    js added

---
+++ javascript/maths.js
@@ -0,0 +1,9 @@
+
+function showDetails(cardId) {
+    const details = document.getElementById(cardId);
+    if (details.style.display === 'none' || details.style.display === '') {
+        details.style.display = 'block';
+    } else {
+        details.style.display = 'none';
+    }
+}
