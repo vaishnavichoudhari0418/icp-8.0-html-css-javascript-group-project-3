98ff752c1a713a620c53b8440ee05bf3cb58723f
Diksha048 <bhagwatdiksha0408@gmail.com>
2024-07-22 01:48:33 +0530

    theme change js added

--- javascript/exam.js
+++ javascript/exam.js
@@ -71,3 +71,23 @@ searchInput.addEventListener('keydown', (event) =>
   }
 }
 );
+/* Theme change */
+const themeIcon = document.getElementById('theme-icon');
+const body = document.body;
+
+themeIcon.addEventListener('click', () =>
+   {
+    if (body.classList.contains('dark-theme'))
+       {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        themeIcon.src = './../../images/arts-page-image/moon.png';
+    } 
+    else
+     {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+        themeIcon.src = './../../images/arts-page-image/sun.png';
+    }
+}
+);
