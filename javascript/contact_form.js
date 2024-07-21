bafe19698bcaea4217cbc6031160971507de845c
khardeaditya <khardeaditya240@gmail.com>
2024-07-21 06:29:42 -0700

    if else ladder used

--- javascript/contact_form.js
+++ javascript/contact_form.js
@@ -3,5 +3,10 @@ document.getElementById('submitBtn').addEventListener('click', function() {
     var email = document.getElementById('email').value;
     var number = document.getElementById('num').value;
     var number = document.getElementById('feedback').value;
-   
+    if (name && email && number && feedback) {
+        alert('Thank you for your message, ' + name + '! We will get back to you soon.');
+        document.getElementById('contactForm').reset();
+    } else {
+        alert('Please fill in all fields before submitting.');
+    }
 });
