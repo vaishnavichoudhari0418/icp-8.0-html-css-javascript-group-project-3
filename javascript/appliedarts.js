9d2b11353dec12dee0abe3f4be44a0eec6e64679
vaishnavichoudhari119@gmail.com <vaishnavichoudhari119@gmail.com>
2024-07-22 00:49:17 +0530

    js read more btn

--- javascript/appliedarts.js
+++ javascript/appliedarts.js
@@ -6,4 +6,16 @@ document.querySelectorAll('.text ul li').forEach(item => {
           scrollToElement.scrollIntoView({ behavior: 'smooth' });
       }
   });
-});
+});  const button = document.getElementById('btn-graduation');
+const tooltip = document.getElementById('tooltip');
+
+button.addEventListener('mouseenter', showTooltip);
+button.addEventListener('mouseleave', hideTooltip);
+
+function showTooltip() {
+    tooltip.style.display = 'block';
+}
+
+function hideTooltip() {
+    tooltip.style.display = 'none';
+}
