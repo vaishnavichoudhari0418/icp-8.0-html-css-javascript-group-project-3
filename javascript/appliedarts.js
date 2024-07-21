75da9fff8ffb742ccbbc50e6cd8c83dec8b68267
vaishnavichoudhari119@gmail.com <vaishnavichoudhari119@gmail.com>
2024-07-21 23:33:18 +0530

    css for scroll down added for cards

--- javascript/appliedarts.js
+++ javascript/appliedarts.js
@@ -1,19 +1,9 @@
-document.getElementById("card-1").addEventListener("click", function() {
-    alert("You clicked on Bachelor of Business Administration (BBA)");
+document.querySelectorAll('.text ul li').forEach(item => {
+  item.addEventListener('click', () => {
+      const scrollToId = item.getAttribute('data-scroll-to');
+      const scrollToElement = document.getElementById(scrollToId);
+      if (scrollToElement) {
+          scrollToElement.scrollIntoView({ behavior: 'smooth' });
+      }
   });
-
-  document.getElementById("card-2").addEventListener("click", function() {
-    alert("You clicked on Bachelor of Hotel Management (BHM)");
-  });
-
-  document.getElementById("card-3").addEventListener("click", function() {
-    alert("You clicked on Bachelor of Mass Communication (BMC)");
-  });
-
-  document.getElementById("card-4").addEventListener("click", function() {
-    alert("You clicked on Bachelor of Social Work (BSW)");
-  });
-
-  document.getElementById("card-5").addEventListener("click", function() {
-    alert("You clicked on Bachelor of Fine Arts (BFA)");
-  });
+});
