f600a0951f22e937778017f43eb004ff733c86c8
khardeaditya <khardeaditya240@gmail.com>
2024-07-21 06:28:58 -0700

    variable declare

--- javascript/contact_form.js
+++ javascript/contact_form.js
@@ -1,16 +1,7 @@
 document.getElementById('submitBtn').addEventListener('click', function() {
-    // Get form values
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var number = document.getElementById('num').value;
-
-    // Check if fields are filled
-    if (name && email && number) {
-        // Display thank you message
-        alert('Thank you for your message, ' + name + '! We will get back to you soon.');
-        // Optionally, clear the form after submission
-        document.getElementById('contactForm').reset();
-    } else {
-        alert('Please fill in all fields before submitting.');
-    }
+    var number = document.getElementById('feedback').value;
+   
 });
