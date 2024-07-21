05cf8f9cfb2cbab47145c926ecc4b60f81484568
vaishnavichoudhari119@gmail.com <vaishnavichoudhari119@gmail.com>
2024-07-22 01:44:01 +0530

    error resolved

--- javascript/appliedarts.js
+++ javascript/appliedarts.js
@@ -1,21 +1,46 @@
-document.querySelectorAll('.text ul li').forEach(item => {
-  item.addEventListener('click', () => {
+document.querySelectorAll('.text ul li').forEach(item => 
+  {
+  item.addEventListener('click', () => 
+    {
       const scrollToId = item.getAttribute('data-scroll-to');
       const scrollToElement = document.getElementById(scrollToId);
       if (scrollToElement) {
           scrollToElement.scrollIntoView({ behavior: 'smooth' });
       }
   });
-});  const button = document.getElementById('btn-graduation');
-const tooltip = document.getElementById('tooltip');
+});  
+document.getElementById('btn-graduation-professional').addEventListener('mouseenter', () => {
+  document.getElementById('tooltip-professional').style.display = 'block';
+});
+document.getElementById('btn-graduation-professional').addEventListener('mouseleave', () => {
+  document.getElementById('tooltip-professional').style.display = 'none';
+});
 
-button.addEventListener('mouseenter', showTooltip);
-button.addEventListener('mouseleave', hideTooltip);
+document.getElementById('btn-graduation-graduation').addEventListener('mouseenter', () => {
+  document.getElementById('tooltip-graduation').style.display = 'block';
+});
+document.getElementById('btn-graduation-graduation').addEventListener('mouseleave', () => {
+  document.getElementById('tooltip-graduation').style.display = 'none';
+});
 
-function showTooltip() {
-    tooltip.style.display = 'block';
-}
+document.getElementById('btn-graduation-degree').addEventListener('mouseenter', () => {
+  document.getElementById('tooltip-degree').style.display = 'block';
+});
+document.getElementById('btn-graduation-degree').addEventListener('mouseleave', () => {
+  document.getElementById('tooltip-degree').style.display = 'none';
+});
 
-function hideTooltip() {
-    tooltip.style.display = 'none';
-}
+document.getElementById('btn-graduation-management').addEventListener('mouseenter', () => {
+  document.getElementById('tooltip-management').style.display = 'block';
+});
+document.getElementById('btn-graduation-management').addEventListener('mouseleave', () => {
+  document.getElementById('tooltip-management').style.display = 'none';
+});
+
+document.getElementById('btn-graduation-architecture').addEventListener('mouseenter', () => {
+  document.getElementById('tooltip-architecture').style.display = 'block';
+});
+document.getElementById('btn-graduation-architecture').addEventListener('mouseleave', () => {
+  document.getElementById('tooltip-architecture').style.display = 'none';
+});
+
