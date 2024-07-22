f23723d119f8f3738682dc549f919c52ff54a68e
mayurkolhe16 <ramchandrakolhe6@gmail.com>
2024-07-22 22:58:33 +0530

    add drak theam

--- javascript/login.js
+++ javascript/login.js
@@ -3,6 +3,40 @@ document.getElementById('dropdown-items').addEventListener('click', function() {
     document.getElementById('navMenu').classList.toggle('active');
 });
 
+/* Theme change */
+const themeIcon = document.getElementById('theme-icon');
+const body = document.body;
+
+themeIcon.addEventListener('click', () =>
+   {
+    if (body.classList.contains('dark-theme'))
+       {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        body.classList.remove('dark-h1')
+        body.classList.add('light-h1')
+        body.classList.remove('dark-writer')
+        body.classList.add('light-writer')
+        body.classList.remove('dark-card')
+        body.classList.add('light-card')
+       
+        themeIcon.src = './../../images/arts-page-image/moon.png';
+    } 
+    else
+     {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+        body.classList.remove('light-h1')
+        body.classList.add('dark-h1')
+        body.classList.remove('light-writer')
+        body.classList.add('dark-writer')
+        body.classList.remove('light-card')
+        body.classList.add('dark-card')
+        themeIcon.src = './../../images/arts-page-image/sun.png';
+    }
+}
+);
+
 
 
 /**login from */
